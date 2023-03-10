52b1cc0466531b02c959dfcfbd62eb3d82dc3598
kaio-nink <kaionink@gmail.com>
2023-03-10 12:50:24 -0300

    Fixing AuthTest
    The missing 'email' parameter from AuthUser class was fixed in auth_test.dart.

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -128,7 +128,10 @@ class MockAuthProvider implements AuthProvider {
     if (password == 'foobar') {
       throw WrongPasswordAuthException();
     }
-    const user = AuthUser(isEmailVerified: false);
+    const user = AuthUser(
+      isEmailVerified: false,
+      email: 'foo@bar.com',
+    );
     _user = user;
     return Future.value(user);
   }
@@ -154,7 +157,10 @@ class MockAuthProvider implements AuthProvider {
     if (user == null) {
       throw UserNotFoundAuthException();
     }
-    const newUser = AuthUser(isEmailVerified: true);
+    const newUser = AuthUser(
+      isEmailVerified: true,
+      email: 'foo@bar.com',
+    );
     _user = newUser;
   }
 }
